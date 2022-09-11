82fe929578e2d0fceaaf4d68610b6927b01cc47f
xaitong <113278412+xaitong@users.noreply.github.com>
2022-09-11 15:38:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,9 +20,9 @@ export const config = {
    *    // 想要发送的人的名字
    *    name: "老婆0",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "oOfK75-_1TVL90LUblKFNZiqROlc",
+   *    id: "oOfK75-T_dGitIxWYNJd1haYYSCw",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "exiqkC3u5_5YtiVHsdP2O11weDa03jSmgDSHp62N5lQ",
+   *    useTemplateId: "YjqCP0XDQOooBWYdGiyWEzGV5HzwXJCn3CDXQyiodcQ",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   },
@@ -33,21 +33,21 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oOfK75-T_dGitIxWYNJd1haYYSCw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "YjqCP0XDQOooBWYdGiyWEzGV5HzwXJCn3CDXQyiodcQ",
       // 所在省份
-      province: "广东",
+      province: "安徽",
       // 所在城市
-      city: "肇庆",
+      city: "安庆",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-26',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
+      festivals: ["type": "*生日", "name": "老婆", "year": "2002", "date": "11-23"，"type": "生日", "name": "老公", "year": "2003", "date": "02-07"
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
@@ -56,7 +56,7 @@ export const config = {
         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
+      customizedDateList: ["keyword": "love_day", date: "2019-07-04"
         // 在一起的日子
         {"keyword": "love_day", date: "2022-09-08"},
         // 结婚纪念日
@@ -156,7 +156,7 @@ export const config = {
    * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
    * 如果为0, 则默认展示全部
    */
-  FESTIVALS_LIMIT: 4,
+  FESTIVALS_LIMIT: 0,
 
   /** 日期相关 */
 
